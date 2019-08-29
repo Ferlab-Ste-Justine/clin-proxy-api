378b72c8c0a321c2f4e224700f3efa16413bd67b
Francois Lachance <latch2112@gmail.com>
2019-08-29 11:50:40 -0400

    fix script

--- redeploy.sh
+++ redeploy.sh
@@ -14,7 +14,9 @@ docker tag localhost:5000/clin-proxy-api-auth-service:latest localhost:5000/clin
 docker push localhost:5000/clin-proxy-api-auth-service:${1}
 docker push localhost:5000/clin-proxy-api-patient-service:${1}
 docker push localhost:5000/clin-proxy-api-variant-service:${1}
-
+docker stack rm qa-proxy-api
+docker stack deploy -c docker-compose.yml qa-proxy-api
+sleep 20
 docker service update qa-proxi-api_auth --image localhost:5000/clin-proxy-api-auth-service:${1}
 docker service update qa-proxi-api_patient --image localhost:5000/clin-proxy-api-patient-service:${1}
 docker service update qa-proxi-api_variant --image localhost:5000/clin-proxy-api-variant-service:${1}
