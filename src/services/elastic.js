78499846fd7eb2843f9393e96f9dd9e288a15231
Francois Lachance <latch2112@gmail.com>
2019-12-17 11:32:02 -0500

    remove console.log

--- src/services/elastic.js
+++ src/services/elastic.js
@@ -173,7 +173,6 @@ export default class ElasticClient {
 
             data.practitionerId = acl.practitioner_id
             data.organizationId = acl.organization_id
-            console.log(`body = ${JSON.stringify(data)}`)
             return rp( {
                 method: 'POST',
                 uri,
