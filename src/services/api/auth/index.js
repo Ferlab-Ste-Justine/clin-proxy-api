0217fc298e45e446497b22a77f49e684a6e3f8bb
Nyan Of The Moon <nyanofthemoon@gmail.com>
2020-04-20 11:01:13 -0400

    auth fuctional tests

--- src/services/api/auth/index.js
+++ src/services/api/auth/index.js
@@ -131,7 +131,6 @@ export default class AuthService extends ApiService {
 
         } ) )
 
-
         // Register Session Route
         this.instance.get( {
             path: `${this.config.endpoint}`
