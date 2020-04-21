58bbaa8c72de2f8dc5e833e7a3f4928b4e4dbf4c
Nyan Of The Moon <nyanofthemoon@gmail.com>
2020-04-21 11:31:20 -0400

    up

--- tests/functional/runner.js
+++ tests/functional/runner.js
@@ -5,12 +5,17 @@ require( 'dotenv' ).config()
 import newman from 'newman'
 
 if ( !process.env.TEST_API_SERVICES ) {
-    console.log( 'No services defined for functional test in TEST_API_SERVICES. Exiting...' )
+    console.error( 'No services defined for functional test in TEST_API_SERVICES. Exiting...' )
     process.exit( 1 )
 }
 
-if ( !process.env.TEST_AUTH_SERVICE_USERNAME || !process.env.TEST_AUTH_SERVICE_PASSWORD ) {
-    console.warn( 'No TEST_AUTH_SERVICE_USERNAME or TEST_AUTH_SERVICE_PASSWORD defined.' )
+if ( process.env.TEST_AUTH_SERVICE_HOSTNAME ) {
+    if ( !process.env.TEST_AUTH_SERVICE_USERNAME || !process.env.TEST_AUTH_SERVICE_PASSWORD ) {
+        console.error( 'No TEST_AUTH_SERVICE_USERNAME or TEST_AUTH_SERVICE_PASSWORD defined. Exiting...' )
+        process.exit( 1 )
+    }
+} else {
+    console.warn( 'No TEST_AUTH_SERVICE_HOSTNAME defined.' )
 }
 
 const enabledServices = JSON.parse( process.env.TEST_API_SERVICES )
