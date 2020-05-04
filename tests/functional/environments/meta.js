351c37e6d4c4e930be35e13b1c4d221a98dc174b
Nyan Of The Moon <nyanofthemoon@gmail.com>
2020-05-04 10:42:21 -0400

    base

---
+++ tests/functional/environments/meta.js
@@ -0,0 +1,54 @@
+const serviceConfig = JSON.parse( process.env.META_API_SERVICE )
+const serviceHostname = process.env.TEST_API_SERVICE_HOSTNAME
+const authServiceConfig = JSON.parse( process.env.AUTH_API_SERVICE )
+const authServiceHostname = process.env.TEST_AUTH_SERVICE_HOSTNAME
+const authUsername = process.env.TEST_AUTH_SERVICE_USERNAME
+const authPassword = process.env.TEST_AUTH_SERVICE_PASSWORD
+
+
+module.exports = {
+    service: 'Meta Service',
+    name: 'meta',
+    values: [
+        {
+            key: 'port',
+            value: serviceConfig.port,
+            enabled: true
+        },
+        {
+            key: 'path',
+            value: serviceConfig.endpoint[ 0 ] === '/' ? serviceConfig.endpoint.substr( 1 ) : serviceConfig.endpoint,
+            enabled: true
+        },
+        {
+            key: 'hostname',
+            value: serviceHostname,
+            enabled: true
+        },
+        {
+            key: 'authPort',
+            value: authServiceConfig.port,
+            enabled: true
+        },
+        {
+            key: 'authPath',
+            value: authServiceConfig.endpoint[ 0 ] === '/' ? authServiceConfig.endpoint.substr( 1 ) : authServiceConfig.endpoint,
+            enabled: true
+        },
+        {
+            key: 'authHostname',
+            value: authServiceHostname,
+            enabled: true
+        },
+        {
+            key: 'authUsername',
+            value: authUsername,
+            enabled: true
+        },
+        {
+            key: 'authPassword',
+            value: authPassword,
+            enabled: true
+        }
+    ]
+}
