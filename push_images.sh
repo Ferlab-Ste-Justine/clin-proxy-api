b40e64fc2eb4f7c84c066f73858071ea42c7245f
Eric Vallee <eric_vallee@webificservices.com>
2020-05-14 12:37:01 -0400

    Feature/ci workflow (#104)
    * Added ci job to ensure the images build
    
    * Typo fix
    
    * Added a pipeline for unit tests
    
    * Unit tests, take 2
    
    * Changed unit tests pipeline to only run unit tests
    
    * Added a script to push images in the ci flow. Removed unit tests from ci flow for now
    
    * Made image publication run for master branch only
    
    * Changed deployment logic to take advantage of images already pushed in the pipeline
    
    * Changed apply logic to prune away previous services that were removed from the orchestration
    
    * Updated README.md

---
+++ push_images.sh
@@ -0,0 +1,19 @@
+export VERSION=$(cat package.json | jq -r ".version")
+
+export AUTH_IMAGE=chusj/clin-proxy-api-auth-service:$VERSION
+export PATIENT_IMAGE=chusj/clin-proxy-api-patient-service:$VERSION
+export VARIANT_IMAGE=chusj/clin-proxy-api-variant-service:$VERSION
+export META_IMAGE=chusj/clin-proxy-api-meta-service:$VERSION
+export GENE_IMAGE=chusj/clin-proxy-api-gene-service:$VERSION
+
+docker build -t $AUTH_IMAGE -f Dockerfile-auth .;
+docker build -t $PATIENT_IMAGE -f Dockerfile-patient .;
+docker build -t $META_IMAGE -f Dockerfile-meta .;
+docker build -t $VARIANT_IMAGE -f Dockerfile-variant .;
+docker build -t $GENE_IMAGE -f Dockerfile-gene .;
+
+docker push $AUTH_IMAGE;
+docker push $PATIENT_IMAGE;
+docker push $META_IMAGE;
+docker push $VARIANT_IMAGE;
+docker push $GENE_IMAGE;
