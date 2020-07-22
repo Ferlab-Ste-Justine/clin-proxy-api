435cf334e4975d2f52c6c7f4196a57c92cba147e
Patrice Laplante <plaplante@ferlab.bio>
2020-07-22 11:59:59 -0400

    Fix cors middleware

--- src/services/api/middleware/cors.js
+++ src/services/api/middleware/cors.js
@@ -1,15 +1,17 @@
-import corsMiddleware from 'restify-cors-middleware'
-
-
 export default ( server, config ) => {
-    const cors = corsMiddleware( config.cors )
 
-    server.pre( cors.preflight )
-    server.use( cors.actual )
+    server.pre( ( req, res, next ) => {
+        const origin = req.header( 'origin' )
+        
+        if ( origin && config.cors && config.cors.origins ){
+            if ( config.cors.origins.includes( origin ) ){
+                res.header( 'Access-Control-Allow-Credentials', true )
+                res.header( 'Access-Control-Allow-Origin', origin )
 
-    server.use( ( req, res, next ) => {
-        res.header( 'Access-Control-Allow-Credentials', true )
-        res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Authorization, Content-Type, Accept' )
+                return next()
+            }
+        }
+
         return next()
     } )
 }
