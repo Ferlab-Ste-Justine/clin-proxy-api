7e29d7e81aab5f65ffce4afa5df9e839b8c578df
YanLabCHU <ymoussaoui@ferlab.bio>
2021-05-06 13:26:51 -0400

    feat: #849 Adds authorization middleware

--- app/src/services/api/helpers/acl.js
+++ app/src/services/api/helpers/acl.js
@@ -13,3 +13,20 @@ export const getACL = ( req ) => {
         roles: req.roles
     }
 }
+
+export const AUTH_RESOURCE_PATIENT_VARIANTS = 'patient-variants'
+export const AUTH_RESOURCE_PATIENT_FAMILY = 'patient-family'
+export const AUTH_RESOURCE_PATIENT_FILES = 'patient-files'
+export const AUTH_RESOURCE_PATIENT_PRESCRIPTIONS = 'patient-prescriptions'
+export const AUTH_RESOURCE_PATIENT_LIST = 'patient-list'
+
+export const AUTH_RESOURCES = [
+    AUTH_RESOURCE_PATIENT_LIST,
+    AUTH_RESOURCE_PATIENT_PRESCRIPTIONS,
+    AUTH_RESOURCE_PATIENT_FILES,
+    AUTH_RESOURCE_PATIENT_FAMILY,
+    AUTH_RESOURCE_PATIENT_VARIANTS
+]
+
+export const KEYCLOAK_AUTH_GRANT_TYPE = 'urn:ietf:params:oauth:grant-type:uma-ticket'
+export const KEYCLOAK_AUTH_RESPONSE_MODE = 'permissions'
